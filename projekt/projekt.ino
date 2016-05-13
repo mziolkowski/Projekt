7d783eb5989ff20eacca002c55d9ff5c09ce2d67
ziolek0321 <ziolek0321@gmail.com>
2016-05-13 18:34:55 +0200

    swiatlo i buzzer
    Dodanie czujnika swiatla oraz buzzera ktory wlacza sie gdy zaczyna
    padac deszcz

--- projekt/projekt.ino
+++ projekt/projekt.ino
@@ -7,10 +7,8 @@
 #define DHTTYPE DHT11
 
 Adafruit_BMP085 bmp;
-
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C lcd(0x27,16,2);
-
   
 void setup() {
   
@@ -27,18 +25,23 @@ void setup() {
 
   //czujnik BMP180
   bmp.begin();
+
+//  if (!bmp.begin()) {
+//  Serial.println("Nie dziala czujnik cisnienia BMP180!!! Sprawdz polaczenie");
+  
  
   Serial.println("Dane odczytane z czujnikow!!!");
 
 }
 
 void loop() {
-  String wilg, wys, cis, temp, opa;
+  String wilg, wys, cis, temp, opa, swi;
   wilg = String ("Wilg: ");
   temp = String ("Temp: ");
   cis = String ("Cis: ");
   wys = String ("Wys: ");
   opa = String ("Opady: ");
+  swi = String ("Swiatlo: ");
   
   //czujnik cisnienia
   float c = bmp.readPressure();
@@ -52,16 +55,21 @@ void loop() {
   //czujnik DHT11
   float h=dht.readHumidity();
   float t=dht.readTemperature();  
+  
+   // czujnik swiatla
+  int fotosensor = analogRead(A1);
+  float s = fotosensor * (100.00 / 1023.0);
 
   String temp1 = (temp + t + "C");
   String wilg1 = (wilg + h + "%");
   String cis1 = (cis + (c/100) + "hPa");
   String opady1 = (opa + o + "%"); 
-  String wys1= (wys + w + "m");
+  String wys1 = (wys + w + "m");
+  String swi1 = (swi + s + "%");
 
   
   //wyswietlenie informacji w knsoli i na lcd
-  if(isnan(t) || isnan(h) || isnan(sensorA) || isnan(sensorD) || isnan(c))
+  if(isnan(t) || isnan(h) || isnan(o) || !bmp.begin())
   {
     Serial.println("Blad odczytu danych z czujnika. Sprawdz wszystkie czujiki!!!");
   }
@@ -72,6 +80,7 @@ void loop() {
     Serial.println(cis1);
     Serial.println(opady1);
     Serial.println(wys1);
+    Serial.println(swi1);
     Serial.println();
     
     lcd.clear();
@@ -108,7 +117,17 @@ void loop() {
     else if (o > 40 && o < 60)
       lcd.print("Zachmurzenie");
     else if (o >= 60)
-      lcd.print("Pada deszcz"); 
+      {
+        tone(12, 2000, 1000);
+        lcd.print("Pada deszcz"); 
+      }  
+    delay(3000);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(swi);
+    lcd.print(s);
+    lcd.print("%");
     delay(3000);
   }
 
